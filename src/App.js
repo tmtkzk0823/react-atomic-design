be27625f20f42d79d73f9c84ce4ea61148433544
tomita_kazuki <96096735+tmtkzk0823@users.noreply.github.com>
2022-09-01 18:43:13 +0900

    Add: Organisimの作成

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
+
+const user = {
+  name: "福沢諭吉",
+  image:
+    "https://images.unsplash.com/photo-1558981403-c5f9899a28bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  email: "000@example.com",
+  phone: "9999-00000",
+  company: {
+    name: "Example株式会社"
+  },
+  website: "httttps://web.com"
+};
 
 export default function App() {
   return (
@@ -10,6 +23,8 @@ export default function App() {
       <SecondaryButton>検索</SecondaryButton>
       <br />
       <SearchInput />
+      <br />
+      <UserCard user={user} />
     </div>
   );
 }
